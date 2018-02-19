1cec87a22eaee08c51d103433e75ea3f98a89a15
wanzhangkai <wanzhangkai@foxmail.com>
2018-02-19 23:04:03 +0800

    update

--- src/data_structure_and_algorithms/binary_search_tree/BSTree.java
+++ src/data_structure_and_algorithms/binary_search_tree/BSTree.java
@@ -58,6 +58,10 @@ public class BSTree<Key extends Comparable<Key>, Value> {
         return node;
     }
 
+    public boolean contain(Key key){
+        return contain(root, key);
+    }
+
     // 查看以node为根的二分搜索树中是否包含键值为key的节点, 使用递归算法
     private boolean contain(Node node, Key key){
 
@@ -70,6 +74,11 @@ public class BSTree<Key extends Comparable<Key>, Value> {
             return contain( node.left , key );
         else // key > node->key
             return contain( node.right , key );
+    }
+
+    // 在二分搜索树中搜索键key所对应的值。如果这个值不存在, 则返回null
+    public Value search(Key key){
+        return search( root , key );
     }
 
     // 在以node为根的二分搜索树中查找key所对应的value, 递归算法
@@ -86,5 +95,48 @@ public class BSTree<Key extends Comparable<Key>, Value> {
         else // key > node->key
             return search( node.right, key );
     }
+
+
+    // 测试二分搜索树
+    public static void main(String[] args) {
+
+        int N = 1000000;
+
+        // 创建一个数组，包含[0...N)的所有元素
+        Integer[] arr = new Integer[N];
+        for(int i = 0 ; i < N ; i ++)
+            arr[i] = new Integer(i);
+
+        // 打乱数组顺序
+        for(int i = 0 ; i < N ; i ++){
+            int pos = (int) (Math.random() * (i+1));
+            Integer t = arr[pos];
+            arr[pos] = arr[i];
+            arr[i] = arr[pos];
+        }
+        // 由于我们实现的二分搜索树不是平衡二叉树，
+        // 所以如果按照顺序插入一组数据，我们的二分搜索树会退化成为一个链表
+        // 平衡二叉树的实现，我们在这个课程中没有涉及，
+        // 有兴趣的同学可以查看资料自学诸如红黑树的实现
+        // 以后有机会，我会在别的课程里向大家介绍平衡二叉树的实现的：）
+
+        // 我们测试用的的二分搜索树的键类型为Integer，值类型为String
+        // 键值的对应关系为每个整型对应代表这个整型的字符串
+        BSTree<Integer,String> bst = new BSTree<Integer,String>();
+        for(int i = 0 ; i < N ; i ++)
+            bst.insert(new Integer(arr[i]), Integer.toString(arr[i]));
+
+        // 对[0...2*N)的所有整型测试在二分搜索树中查找
+        // 若i在[0...N)之间，则能查找到整型所对应的字符串
+        // 若i在[N...2*N)之间，则结果为null
+        for(int i = 0 ; i < 2*N ; i ++){
+            String res = bst.search(new Integer(i));
+            if( i < N )
+                assert res.equals(Integer.toString(i));
+            else
+                assert res == null;
+        }
+    }
+
 
 }
