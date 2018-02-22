ee29066be56efa3aa186f77488c2318ede6e91da
wanzhangkai <wanzhangkai@foxmail.com>
2018-02-22 20:20:04 +0800

    add multithreading

---
+++ src/multithreading/ThreadDemo1.java
@@ -0,0 +1,70 @@
+package multithreading;
+
+/**
+ * 多线程简单demo
+ *
+ * @author wanzhangkai@foxmail.com
+ * @date 2018/2/22 17:33
+ */
+public class ThreadDemo1 {
+
+    static class Thread1 extends Thread {
+
+        public void run() {
+            System.out.println(getName() + "is a actor!");
+            int count = 0;
+            boolean keepRunning = true;
+
+            while (keepRunning) {
+                System.out.println(getName() + " show：" + (++count));
+                if (count == 100) {
+                    keepRunning = false;
+                }
+                if (count % 10 == 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.out.println("sleep() exception");
+                    }
+                }
+            }
+            System.out.println("Show Over!");
+        }
+    }
+
+    //notice the different between two pattern
+    static class Thread2 implements Runnable {
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + "is a actor!");
+            int count = 0;
+            boolean keepRunning = true;
+
+            while (keepRunning) {
+                System.out.println(Thread.currentThread().getName() + " show：" + (++count));
+                if (count == 100) {
+                    keepRunning = false;
+                }
+                if (count % 10 == 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        System.out.println("sleep() exception");
+                    }
+                }
+            }
+            System.out.println("Show Over!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread1();
+        thread1.setName("thread1");
+        Thread thread2 = new Thread(new Thread2(), "thread2");
+        thread1.start();
+        thread2.start();
+    }
+
+
+}
