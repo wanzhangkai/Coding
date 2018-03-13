249118e79c997019f42e739e8f9a114233aae77a
wanzhangkai <wanzhangkai@foxmail.com>
2018-03-13 21:42:12 +0800

    update

--- src/sword_to_offer/Q17_MergeSortedLinkedLists.java
+++ src/sword_to_offer/Q17_MergeSortedLinkedLists.java
@@ -1,5 +1,8 @@
 package sword_to_offer;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 合并两个排序的链表：
  * 输入两个单调递增的链表头结点，输出两个链表合成后的链表头结点，
@@ -15,54 +18,8 @@ public class Q17_MergeSortedLinkedLists {
         ListNode next;
     }
 
-    public ListNode solution(ListNode head1, ListNode head2) {
-
-        ListNode newHead = null;
-        ListNode currentNode = null;
-        if (head1 == null && head2 == null) {
-            return null;
-        }
-        if (head1 == null) {
-            currentNode = head2;
-        } else if (head2 == null) {
-            currentNode = head1;
-        }else {
-            if (head1.val < head2.val) {
-                currentNode = head1;
-                head1 = head1.next;
-            } else {
-                currentNode = head2;
-                head2 = head2.next;
-            }
-        }
-        newHead = currentNode;
-
-        while (head1 != null && head2 != null) {
-            if (head1.val < head2.val) {
-                currentNode.next = head1;
-                currentNode = currentNode.next;
-                head1 = head1.next;
-            } else {
-                currentNode.next = head2;
-                currentNode = currentNode.next;
-                head2 = head2.next;
-            }
-        }
-        while (head1 != null) {
-            currentNode.next = head1;
-            currentNode = currentNode.next;
-            head1 = head1.next;
-        }
-        while (head2 != null) {
-            currentNode.next = head2;
-            currentNode = currentNode.next;
-            head2 = head2.next;
-        }
-        return newHead;
-    }
-
     //递归
-    public ListNode solution2(ListNode head1, ListNode head2) {
+    public ListNode solution(ListNode head1, ListNode head2) {
         if (head1 == null) {
             return head2;
         }
@@ -73,12 +30,62 @@ public class Q17_MergeSortedLinkedLists {
         ListNode newHead = null;
         if (head1.val <= head2.val) {
             newHead = head1;
-            newHead.next = solution2(head1.next, head2);
+            newHead.next = solution(head1.next, head2);
         } else {
             newHead = head2;
-            newHead.next = solution2(head1, head2.next);
+            newHead.next = solution(head1, head2.next);
         }
         return newHead;
+    }
+
+    public ListNode solution2(ListNode head1, ListNode head2) {
+        return null;
+//        ListNode newHead = null;
+//        ListNode currentNode = null;
+//        if (head1 == null && head2 == null) {
+//            return null;
+//        }
+//        if (head1 == null) {
+//            currentNode = head2;
+//        } else if (head2 == null) {
+//            currentNode = head1;
+//        }else {
+//            if (head1.val < head2.val) {
+//                currentNode = head1;
+//                head1 = head1.next;
+//            } else {
+//                currentNode = head2;
+//                head2 = head2.next;
+//            }
+//        }
+//        newHead = currentNode;
+//
+//        while (head1 != null && head2 != null) {
+//            if (head1.val < head2.val) {
+//                currentNode.next = head1;
+//                currentNode = currentNode.next;
+//                head1 = head1.next;
+//            } else {
+//                currentNode.next = head2;
+//                currentNode = currentNode.next;
+//                head2 = head2.next;
+//            }
+//        }
+//        while (head1 != null) {
+//            currentNode.next = head1;
+//            currentNode = currentNode.next;
+//            head1 = head1.next;
+//        }
+//        while (head2 != null) {
+//            currentNode.next = head2;
+//            currentNode = currentNode.next;
+//            head2 = head2.next;
+//        }
+//        return newHead;
+    }
+
+    public static void main(String[] args) {
+        List list = new LinkedList<>();
     }
 
 }
---
+++ src/sword_to_offer/Q18_HasSubtree.java
@@ -0,0 +1,17 @@
+package sword_to_offer;
+
+/**
+ * 树的子结构:
+ * 输入两棵二叉树A，B，判断B是不是A的子结构。
+ * （ps：我们约定空树不是任意一个树的子结构）
+ *
+ * @author wanzhangkai@foxmail.com
+ * @date 2018/3/13 21:24
+ */
+public class Q18_HasSubtree {
+
+
+
+
+
+}
